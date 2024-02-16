98ca579176ca90d4b32594a33b2192e42eca7a9f
Monika <monika.belkaite@gmail.com>
2024-02-16 21:59:06 +0200

    feat: add first version of upload feature

--- src/components/UploadImage.vue
+++ src/components/UploadImage.vue
@@ -1,11 +1,44 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue';
+import { getStorage, ref as firebaseStorageRef, uploadBytes } from 'firebase/storage';
+
+
+const storage = getStorage();
+const selectedFile = ref<File | null>(null);
+
+
+const handleFileSelect = (event: Event) => {
+  const input = event.target as HTMLInputElement;
+  if (input.files) {
+    [selectedFile.value] = input.files;
+  }
+};
+
+
+const uploadImage = async () => {
+  if (selectedFile.value) {
+    const imageRef = firebaseStorageRef(storage, `images/${selectedFile.value.name}`);
+    try {
+      console.log('Starting upload...');
+      const snapshot = await uploadBytes(imageRef, selectedFile.value);
+      console.log('Upload successful!', snapshot);
+    } catch (error) {
+      console.error('Upload failed', error);
+    }
+  } else {
+    console.log('No file selected');
+  }
+};
+</script>
 
 <template>
-  <div></div>
+  <div>
+    <label for="fileInput">Upload Image</label>
+    <input type="file" id="fileInput" @change="handleFileSelect" />
+    <button type="button" @click="uploadImage">Upload Image</button>
+  </div>
 </template>
 
 <style scoped>
-h3 {
-  font-size: 1.2rem;
-}
+/* styles */
 </style>
--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -2,11 +2,11 @@ import { initializeApp } from 'firebase/app';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: import.meta.env.FIREBASE_API_KEY,
-  authDomain: import.meta.env.FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.FIREBASE_STORAGE_BUCKET,
-  appId: import.meta.env.FIREBASE_APP_ID
+  apiKey: import.meta.env.VITE_FIREBASE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_FIREBASE_STORAGE_BUCKET,
+  appId: import.meta.env.VITE_FIREBASE_FIREBASE_APP_ID
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
