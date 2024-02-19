0bfef98c837bbd85e50812b33cdfba82eb997a87
Monika <monika.belkaite@gmail.com>
2024-02-19 18:31:46 +0200

    refactor: add type annotations in Image List component

--- src/components/ImageList.vue
+++ src/components/ImageList.vue
@@ -3,29 +3,31 @@ import { ref, computed, onMounted } from 'vue';
 import { getStorage, ref as firebaseRef, list, getDownloadURL } from 'firebase/storage';
 
 const imagesListRef = firebaseRef(getStorage(), 'images');
-// Adjusting the structure to store both URLs and file names
-const images = ref([]);
-const pageToken = ref(null);
+type ImageInfo = {
+  url: string;
+  name: string;
+};
+
+const images = ref<ImageInfo[]>([]);
+const pageToken = ref<string | undefined>();
+const errorMessage = ref('');
 
 const fetchImages = async () => {
   try {
     const result = await list(imagesListRef, { maxResults: 10, pageToken: pageToken.value });
-    // Create an array of promises to get both URLs and names
     const imagePromises = result.items.map(async (itemRef) => ({
       url: await getDownloadURL(itemRef),
-      name: itemRef.name // Get the file name from the reference
+      name: itemRef.name
     }));
-
     const fetchedImages = await Promise.all(imagePromises);
     images.value.push(...fetchedImages);
-
     pageToken.value = result.nextPageToken;
   } catch (error) {
-    console.error('Error fetching images:', error);
+    errorMessage.value = `Error fetching images: ${error}`;
   }
 };
 
-const canFetchMore = computed(() => !!pageToken.value);
+const canFetchMore = computed(() => Boolean(pageToken.value));
 
 onMounted(fetchImages);
 </script>
@@ -38,6 +40,7 @@ onMounted(fetchImages);
         <img :src="image.url" :alt="image.name" class="uploaded-image" />
       </div>
     </div>
+    <div v-if="errorMessage"> {{ errorMessage }}</div>
     <button type="button" v-if="canFetchMore" @click="fetchImages" class="load-more-button">
       Load more
     </button>
